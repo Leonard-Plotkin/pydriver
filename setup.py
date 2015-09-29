d9f10e2521cba37e3d3220d5acfc70dfc1276029
Leonard Plotkin <git@leonard-plotkin.de>
2015-09-29 23:28:17 +0200

    Add Python 3 classifier in setup.py

--- setup.py
+++ setup.py
@@ -263,6 +263,7 @@ setup_args = {
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Cython',
         'Programming Language :: C++',
         'Topic :: Scientific/Engineering :: Image Recognition',
